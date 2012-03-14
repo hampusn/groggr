59333184924ff26974ab12937ec8fce75c7a64cd
Hampus Nordin <nordin.hampus@gmail.com>
2012-03-14 18:53:06 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Groggr::Application.config.secret_token = '91f253ea1a12d5b14f92e8e45558963054296eca22c936c1fb2bce6e122e6523f04fdc3f3ba474c1fcb41ef3e7586ab8be284357a5c73735beff63a9a1597ed1'
